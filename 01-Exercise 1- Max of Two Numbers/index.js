97f48e27372e3e3f55016be55f6a75c47af0593f
apple <apple@apples-MacBook-Air.local>
2022-02-17 18:19:34 +0500

    New: Exercise 1

---
+++ 01-Exercise 1- Max of Two Numbers/index.js
@@ -0,0 +1,8 @@
+// Write a Function that takes two numbers and returns the maximum of two.
+
+function maxTwoNum(num1, num2) {
+    return num1 > num2 ? num1 : num2
+}
+
+let num = maxTwoNum(2,10);
+console.log(num);
---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript Excercises By Muhammad Shakir Khan
+
+## Excercises
+1. Exercise 1- Max of Two Numbers
